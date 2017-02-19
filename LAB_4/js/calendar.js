d7b0b21f850c14a7b5f6d810f0ed8ba63a9b1e63
Derthy <kapj-44@sr.dmcs.pl>
2017-02-19 14:10:52 +0100

    timetable ready

--- LAB_4/js/calendar.js
+++ LAB_4/js/calendar.js
@@ -27,8 +27,11 @@ $(document).ready(function(){
             $('#EndHour').append("<option>"+i+"</option>");
     }
 
+    for (var i=0; i<7; i++){
+        $('#Day').append("<option>"+days[i]+"</option>");
+    }
+
     createTable();
-    addElement();
 
 });
 
@@ -43,10 +46,7 @@ function createTable(){
 
             var td = document.createElement('td');
             if((i==0 && j>7)){
-                if(j>=10)
-                    td.appendChild(document.createTextNode(j+':00'));
-                else
-                    td.appendChild(document.createTextNode('0'+j+':00'));
+                td.appendChild(document.createTextNode(j+':15'));
             }
             else if((j==7 && i>0)){
                 td.appendChild(document.createTextNode(days[i-1]));
@@ -61,5 +61,20 @@ function createTable(){
 }
 
 function addElement(){
+    var lenght = document.getElementById("EndHour").value - document.getElementById("BeginHour").value;
+    var cell = days.indexOf(document.getElementById("Day").value)+1;
+    var margin = (document.getElementById("BeginHour").value - 8)*300;
+    //document.getElementById("2x8").innerHTML = cell;
+
+    var htmlValue = document.getElementById(cell+"x8").outerHTML;
+
+    if(lenght>0)
+    document.getElementById(cell+"x8").innerHTML = htmlValue + "<button id=\"added\" style=\"width:"+(lenght*300)+"px;margin-left:"+margin+"px\" type=\"button\" class=\"btn btn-primary\">Save changes</button>";
+}
+
+function tmp(){
     document.getElementById("2x8").innerHTML = "<button style=\"width: 600px\" type=\"button\" class=\"btn btn-primary\">Save changes</button><button style=\"width: 600px;margin-left:620px\" type=\"button\" class=\"btn btn-primary\">Save changes</button>";
+    var value = document.getElementById("2x8").outerHTML;
+    document.getElementById("2x8").innerHTML = value + "<button id=\"added\" style=\"width: 600px;margin-left:620px\" type=\"button\" class=\"btn btn-primary\">Save changes</button>";
+    document.getElementById("added").remove();
 }
